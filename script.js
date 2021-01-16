74a30012074c491f8bcda10029747997aa1b5e5e
jkelly101 <jenLkelly@gmail.com>
2021-01-16 18:16:33 -0500

    re-structured html and updated var names to be clearer and easier to understand

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
 // # PseudoCode
 
-// ```
+// Variables Here
 
-var startBtn = document.querySelector("#start");
+var startBtn = document.querySelector("#start-button");
 var startScreen = document.querySelector("#start-screen");
 var qScreen = document.querySelector("#question-screen");
-var qBtns = document.querySelectorAll(".btn");
-var questionEl = document.querySelector("#question");
-var rightAns = document.querySelector("#rightOrWrong");
-var ansIndex = document.querySelector("#answers");
+var qText = document.querySelector("#question-text");
+var aBtns = document.querySelectorAll("#answer-buttons");
 
 var qIndex = [
   //question 1 / index 0
   {
     q: "1. An array is enclosed within which type of brackets?",
-    a: "[ ]",
     o: ["{ }", "[ ]", "( )", "< >"],
   },
   //question 2 / index 1
   {
     q: "2. In a for loop, how do you increment i by 1?",
-    a: "i++",
     o: ["i = 0", "i++", "i + 1", "i < 0"],
   },
   //question 3 / index 2
   {
     q: "3. How do you call a function named myFunction?",
-    a: "myFunction()",
     o: [
       "myFunction()",
       "function myFunction",
@@ -37,7 +32,6 @@ var qIndex = [
   //question 4 / index 3
   {
     q: "4. Where is the correct place to insert a Javascript?",
-    a: "The <head> or the <body>",
     o: [
       "The <head>",
       "The <body>",
@@ -48,15 +42,22 @@ var qIndex = [
   //question 5 / index 4
   {
     q: "5. Which of these options will return a random integer?",
-    a: "Math.floor(Math.Random)",
     o: ["Math.floor", "Math.float", "Math.random", "Math.floor(Math.random)"],
   },
   //question 6 / index 5
   {
-    q: "Which of the following wouls you use to say i is not equal to 5?",
-    a: "i != 5",
+    q: "Which of the following would you use to say i is not equal to 5?",
     o: ["i == 5", "i != 5", "i =! 5", "i >= 5"],
   },
+];
+
+var qAnswers = [
+  "[ ]",
+  "i++",
+  "myFunction()",
+  "<The <head> or the <body>",
+  "Math.floor(Math.random)",
+  "i != 5",
 ];
 
 // GIVEN I am taking a code quiz
@@ -77,25 +78,9 @@ startBtn.addEventListener("click", startQuiz);
 
 // question shows up on the page
 // show the multiple choice answers
-function displayQuestion() {
-  qScreen.style.display = "block";
-
-  for (var i = 0; i < qIndex.length; i++) {
-    qBtns[i].textContent = qIndex[0].o[i];
-    questionEl.textContent = qIndex[0].q;
-  }
-}
 
 // click handler is triggered on what the user clicked on
 // shows if the answer is correct or not correct
-
-ansIndex.addEventListener("click", function (event) {
-  var element = event.target;
-
-  if (element === qIndex[i].a) {
-    rightAns.innerHTML = "Correct!";
-  }
-});
 
 // WHEN I answer a question
 // THEN I am presented with another question
