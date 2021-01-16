80253240392821c18c2a753ab14f8c2ad9a00b59
jkelly101 <jenLkelly@gmail.com>
2021-01-16 00:02:54 -0500

    finished questions array. added for loop for questions/answer 0

--- script.js
+++ script.js
@@ -5,30 +5,57 @@
 var startBtn = document.querySelector("#start");
 var startScreen = document.querySelector("#start-screen");
 var qScreen = document.querySelector("#question-screen");
-var answers = document.querySelectorAll(".btn");
-var questions = document.querySelector("#question");
+var qBtns = document.querySelectorAll(".btn");
+var questionEl = document.querySelector("#question");
+var rightAns = document.querySelector("#rightOrWrong");
 
 var qIndex = [
   //question 1 / index 0
   {
-    q: "An array is enclosed within which type of brackets?",
-    a: "[]",
-    o: ["{}", "[]", "()", "//"],
+    q: "1. An array is enclosed within which type of brackets?",
+    a: "[ ]",
+    o: ["{ }", "[ ]", "( )", "< >"],
   },
   //question 2 / index 1
   {
-    q: "In a for loop, how do you increment i by 1?",
+    q: "2. In a for loop, how do you increment i by 1?",
     a: "i++",
     o: ["i = 0", "i++", "i + 1", "i < 0"],
   },
   //question 3 / index 2
-  { q: "", a: "", o: ["1", "2", "3", "4"] },
+  {
+    q: "3. How do you call a function named myFunction?",
+    a: "myFunction()",
+    o: [
+      "myFunction()",
+      "function myFunction",
+      "function = myFunction()",
+      "call myFunction",
+    ],
+  },
   //question 4 / index 3
-  { q: "", a: "", o: ["1", "2", "3", "4"] },
+  {
+    q: "4. Where is the correct place to insert a Javascript?",
+    a: "The <head> or the <body>",
+    o: [
+      "The <head>",
+      "The <body>",
+      "The <head> or the <body>",
+      "The <document>",
+    ],
+  },
   //question 5 / index 4
-  { q: "", a: "", o: ["1", "2", "3", "4"] },
+  {
+    q: "5. Which of these options will return a random integer?",
+    a: "Math.floor(Math.Random)",
+    o: ["Math.floor", "Math.float", "Math.random", "Math.floor(Math.random)"],
+  },
   //question 6 / index 5
-  { q: "", a: "", o: ["1", "2", "3", "4"] },
+  {
+    q: "Which of the following wouls you use to say i is not equal to 5?",
+    a: "i != 5",
+    o: ["i == 5", "i != 5", "i =! 5", "i >= 5"],
+  },
 ];
 
 // GIVEN I am taking a code quiz
@@ -36,30 +63,39 @@ var qIndex = [
 // THEN a timer starts and I am presented with a question
 // click handler is triggered on the start button
 // timer starts
-// question shows up on the page
-// show the multiple choice answers
 
 function startQuiz() {
-  startScreen.style.display = "none";
-  displayQuestion();
-
   // hide the start page
+  startScreen.style.display = "none";
   // call display question function
+  displayQuestion();
   // start timer
 }
 
+startBtn.addEventListener("click", startQuiz);
+
+// question shows up on the page
+// show the multiple choice answers
 function displayQuestion() {
   qScreen.style.display = "block";
 
-  // remove display: none to show first question
+  for (var i = 0; i < qIndex.length; i++) {
+    qBtns[i].textContent = qIndex[0].o[i];
+    questionEl.textContent = qIndex[0].q;
+  }
 }
 
-startBtn.addEventListener("click", startQuiz);
+qBtns.onclick = function checkAns(event) {
+  if (event.target.matches(qIndex[i].a)) {
+    rightAns.innerHTML = "Correct!";
+  }
+};
 
 // WHEN I answer a question
 // THEN I am presented with another question
 // click handler is triggered on what the user clicked on
 // shows if the answer is correct or not correct
+
 // remove the last question
 // remove the last answers
 // after a short time,
